128ad701b8ed31dde402958b07ef5bfb8ae2fff3
git-riyanhidayat <95109422+git-riyanhidayat@users.noreply.github.com>
2021-12-02 08:57:31 +0700

    Create Find Item in the meeting room.js

---
+++ JSON Manipulation/Find Item in the meeting room.js
@@ -0,0 +1,128 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Find Item in the meeting room</title>
+</head>
+<body>
+    <script>
+  
+    var json = `{
+        "meetingroom":   {
+    "inventory_id": 9380,
+    "name": "Big Desk",
+    "type": "furniture",
+    "tags": [
+      "desk",
+      "furniture",
+      "brown"
+    ],
+    "purchased_at": 1579190642,
+    "placement": {
+      "room_id": 3,
+      "name": "Meeting Room"
+    }
+  },
+    "inventory_id": 9380,
+    "name": "Big Desk",
+    "type": "furniture",
+    "tags": [
+      "desk",
+      "furniture",
+      "brown"
+    ],
+    "purchased_at": 1579190642,
+    "placement": {
+      "room_id": 3,
+      "name": "Meeting Room"
+}
+
+
+
+    }`;
+    
+    // Mengubah JSON ke JS 
+    var obj = JSON.parse(json);
+    
+    fun<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Parse Nested JSON Data in JavaScript</title>
+</head>
+<body>
+    <script>
+  
+    var json = `{
+        "meetingroom":   {
+    "inventory_id": 9380,
+    "name": "Big Desk",
+    "type": "furniture",
+    "tags": [
+      "desk",
+      "furniture",
+      "brown"
+    ],
+    "purchased_at": 1579190642,
+    "placement": {
+      "room_id": 3,
+      "name": "Meeting Room"
+    }
+  },
+    "inventory_id": 9380,
+    "name": "Big Desk",
+    "type": "furniture",
+    "tags": [
+      "desk",
+      "furniture",
+      "brown"
+    ],
+    "purchased_at": 1579190642,
+    "placement": {
+      "room_id": 3,
+      "name": "Meeting Room"
+}
+
+
+
+    }`;
+    
+    var obj = JSON.parse(json);
+    
+    function printValues(obj) {
+        for(var k in obj) {
+            if(obj[k] instanceof Object) {
+                printValues(obj[k]);
+            } else {
+                document.write(obj[k] + "<br>");
+            };
+        }
+    };
+    
+    printValues(obj);
+    
+    document.write("<hr>");
+    
+    document.write(obj["meetingroom"]["tags"] + "<br>");  
+
+    </script>
+</body>
+</html>ction printValues(obj) {
+        for(var k in obj) {
+            if(obj[k] instanceof Object) {
+                printValues(obj[k]);
+            } else {
+                document.write(obj[k] + "<br>");
+            };
+        }
+    };
+    
+    printValues(obj);
+    
+    document.write("<hr>");
+    
+    document.write(obj["meetingroom"]["tags"] + "<br>");  
+
+    </script>
+</body>
+</html>
